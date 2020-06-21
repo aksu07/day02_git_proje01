cfdf3522ad973f2b2c59cc6549083c1ebf3d6a64
aksu07 <abdullatifaksu07@gmail.com>
2020-06-21 16:34:18 -0400

    bu gunku son commit

---
+++ src/package01/Notes.java
@@ -0,0 +1,52 @@
+package package01;
+
+public class Notes {
+
+		/*https://github.com/salptekin
+		Bu projeyi nasil dogrudan eclipse ime tasiyabilir?
+		https://github.com/HalilIbrahimHasan/dummy_project.git
+		https://github.com/CevdetTellioglu/com.techproedproject.git
+		Git Url alindiktan sonra => https://github.com/CevdetTellioglu/com.techproedproject.git
+		eclipse e gidiyoruz
+		File a tikliyoruz
+		import diyoruz
+		Git e tikliyoruz
+		Projects from git
+		Next diyoruz
+		Clone URI
+		Next
+		Next
+		Next
+		Next
+		Eger proje hali hazirda sizde varda (PC de bulunuyorsa)
+		import existing eclipse projects
+		Eger proje sizde yoksa 
+		import as general project e tiklayiniz
+		Next 
+		Finish
+		GitHub Day2
+		git add . => her seyi ekliyor => Staging area
+		git commit -m "mesajimizi belirtiyoruz" => local repo da
+		git push => tum kodlarimiz remote repoya gidiyor
+		GitHub dan gitHub a yollamak icin fork etmeniz gerekiyor
+		Branching diye bir sey vardir
+		master -> master => yazmis oldugum kodlari remote a yolladigimda 
+			local master branchimden (dal) => remote master branchime yolluyorum
+		localdaki branchi gormek icin 
+		git branch => yaziyoruz
+		git branch isim => o ana kadar ne kadar commitiniz varsa master branchte onlar kopyalanir 
+			ve yeni olusturduunuz branche eklenir
+		Eger hangi branch te (dalda) oldugumuzu gormek istersek 
+		git branch => branch ismini bir yildizla gosterir
+		Eger branchinizi degistirmek isterseniz 
+		git checkout branch isimi => bu sizi yazdiginiz branche goturur.
+		Eger branch degisikligi yaparsaniz 
+		Switched to branch 'feature'
+		bu ifadeyi gorursunuz
+		git push --set-upstream origin feature ilk defaya mahsus 
+		local (yerel) feature branchinizden remote(GitHub )taki feature branchinize degisiklikleri aktariyorsunuz*/
+
+	
+	
+	
+}
